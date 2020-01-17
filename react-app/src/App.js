ea856b4a03a1e557509313184407f3ddee9cbf59
Daniel Velázquez <dvelazquez@emergya.com>
2020-01-17 11:25:17 +0100

    paginación

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,20 +6,30 @@ import AddIcon from '@material-ui/icons/Add';
 import './App.css';
 import BookCard from './components/book-card/BookCard';
 import BookModal from './components/book-modal/BookModal';
+import ReactPaginate from 'react-paginate';
 
 class App extends Component { 
 
-
-  books = [
+  allBooks = [
     {user: 'John Doe', type: 0, title: 'title 1', writter: 'escritor1', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet lectus lectus, non laoreet lorem ornare sit amet. Maecenas condimentum felis quis nunc pretium, sed lacinia ipsum molestie. Donec dapibus orci id dolor porta consectetur'},
     {user: 'Kenny Goodwin', type: 1, title: 'title 2', writter: 'escritor2', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet lectus lectus, non laoreet lorem ornare sit amet. Maecenas condimentum felis quis nunc pretium, sed lacinia ipsum molestie. Donec dapibus orci id dolor porta consectetur'},
     {user: 'Lesly Arsenault', type: 1, title: 'title 3', writter: 'escritor3', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet lectus lectus, non laoreet lorem ornare sit amet. Maecenas condimentum felis quis nunc pretium, sed lacinia ipsum molestie. Donec dapibus orci id dolor porta consectetur'},
     {user: 'Arlene Halsey', type: 1, title: 'title 4', writter: 'escritor4', description: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet lectus lectus, non laoreet lorem ornare sit amet. Maecenas condimentum felis quis nunc pretium, sed lacinia ipsum molestie. Donec dapibus orci id dolor porta consectetur'},
-  ]
+  ]  
+
+  books = [];
+
+  constructor(props) {
+    super(props)  
+    this.state = {
+      offset: 0,
+      books: this.allBooks.slice(0, 2)
+    };
+  }
 
   booksList = () => (
       <div>
-      {this.books.map((book) => 
+      {this.state.books.map((book) => 
         <BookCard book={book}/>
       )}
       </div>
@@ -28,6 +38,14 @@ class App extends Component {
   handleOpen() {
     this.refs.modal.handleOpen();
   }
+
+  handlePageClick = data => {    
+    let selected = data.selected;
+    let offset = Math.ceil(selected * 2);    
+    this.setState({ offset: offset }, () => {
+      this.setState({books: this.allBooks.slice(offset, offset + 2)})
+    });
+  };
 
   
   render() {
@@ -39,7 +57,19 @@ class App extends Component {
           <Fab className="add-button" color="primary" aria-label="add" onClick={this.handleOpen.bind(this)}>
             <AddIcon />
           </Fab>
-        </Container>
+          <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={2}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            subContainerClassName={'pages pagination'}
+            activeClassName={'active'}/>
+        </Container>      
         <BookModal ref="modal"/>
       </React.Fragment>    
     );
